51e3f9b3e6b1eacf8a49dbb8b875f3af74eff882
efrain-vasquez <efrain.vasquez.arias777@gmail.com>
2019-05-28 16:25:30 -0500

    update to input in LoanAgreement component

--- client/src/components/LoanAgreementTerms.jsx
+++ client/src/components/LoanAgreementTerms.jsx
@@ -51,33 +51,39 @@ class LoanAgreementTerms extends Component {
   render () {
     const { LoanTermsInfo } = this.props
     const IndividualLoanTerms = ({ id, Interest_On_Loan, Repayment_Schedule, Number_Of_Payments, Lenders_Pay_Pal_Info }) =>
-      (<tr><td>{`${id}`}</td><td>
+      (<tr>
+        <td>
+          {`${id}`}
+        </td>
 
-        <div class='field'>
-          <div class='control'>
-            <div class='select is-primary'>
-              <select
-                class='input is-hovered'
-                placeholder='Hovered input'
-                name='Interest_On_Loan'
-                value={Interest_On_Loan}
-                onChange={this.handleLoanTermsInput}>
-                <option value='5 Percent'>5 Percent</option>
-                <option value='10 Percent'>10 Percent</option>
-                <option value='15 Percent'>15 Percent</option>
-                <option value='20 Percent'>20 Percent</option>
-                <option value='25 Percent'>25 Percent</option>
-              </select>
+        <td>
+          <div className='field'>
+            <div className='control'>
+              <div className='select is-primary'>
+                <select
+                  className='input is-hovered'
+                  placeholder='Hovered input'
+                  name='Interest_On_Loan'
+                  value={Interest_On_Loan}
+                  onChange={this.handleLoanTermsInput}>
+                  <option value='5 Percent'>5 Percent</option>
+                  <option value='10 Percent'>10 Percent</option>
+                  <option value='15 Percent'>15 Percent</option>
+                  <option value='20 Percent'>20 Percent</option>
+                  <option value='25 Percent'>25 Percent</option>
+                </select>
+              </div>
             </div>
           </div>
-        </div>
-        {`${Interest_On_Loan}`}</td><td>
+          {`${Interest_On_Loan}`}
+        </td>
 
-          <div class='field'>
-          <div class='control'>
-              <div class='select is-primary'>
-              <select
-                  class='input is-hovered'
+        <td>
+          <div className='field'>
+            <div className='control'>
+              <div className='select is-primary'>
+                <select
+                  className='input is-hovered'
                   placeholder='Hovered input'
                   name='Repayment_Schedule'
                   value={Repayment_Schedule}
@@ -87,37 +93,49 @@ class LoanAgreementTerms extends Component {
                   <option value='Monthly'>Monthly</option>
                   <option value='One_Lump_Sum'>One Lump Sum</option>
                 </select>
-            </div>
-            </div>
-        </div>
-        {`${Repayment_Schedule}`}</td><td>
-
-        <div class='field'>
-            <div class='control'>
-            <div class='select is-primary'>
-                <select
-                class='input is-hovered'
-                placeholder='Hovered input'
-                name='Number_Of_Payments'
-                value={Number_Of_Payments}
-                onChange={this.handleLoanTermsInput}>
-                <option value='1 Payment'>1 Payment</option>
-                <option value='2 Payments'>2 Payments</option>
-                <option value='3 Payments'>3 Payments</option>
-                <option value='4 Payments'>4 Payments</option>
-                <option value='5 Payments'>5 Payments</option>
-                <option value='6 Payments'>6 Payments</option>
-                <option value='7 Payments'>7 Payments</option>
-                <option value='8 Payments'>8 Payments</option>
-                <option value='9 Payments'>9 Payments</option>
-                <option value='10 Payments'>10 Payments</option>
-              </select>
               </div>
+            </div>
           </div>
-          </div>
-        {`${Number_Of_Payments}`}</td>
+          {`${Repayment_Schedule}`}
+        </td>
 
-        <td>{`${Lenders_Pay_Pal_Info}`}</td></tr>)
+        <td>
+          <div className='field'>
+            <div className='control'>
+              <div className='select is-primary'>
+                <select
+                  className='input is-hovered'
+                  placeholder='Hovered input'
+                  name='Number_Of_Payments'
+                  value={Number_Of_Payments}
+                  onChange={this.handleLoanTermsInput}>
+                  <option value='1 Payment'>1 Payment</option>
+                  <option value='2 Payments'>2 Payments</option>
+                  <option value='3 Payments'>3 Payments</option>
+                  <option value='4 Payments'>4 Payments</option>
+                  <option value='5 Payments'>5 Payments</option>
+                  <option value='6 Payments'>6 Payments</option>
+                  <option value='7 Payments'>7 Payments</option>
+                  <option value='8 Payments'>8 Payments</option>
+                  <option value='9 Payments'>9 Payments</option>
+                  <option value='10 Payments'>10 Payments</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          {`${Number_Of_Payments}`}
+        </td>
+
+        <td>
+          <input
+            className='input is-hovered is-primary'
+            type='text'
+            name='Lenders_Pay_Pal_Info'
+            value={Lenders_Pay_Pal_Info}
+            onChange={this.handleLoanTermsInput} />
+          {`${Lenders_Pay_Pal_Info}`}
+        </td>
+      </tr>)
 
     return (
       <div>
